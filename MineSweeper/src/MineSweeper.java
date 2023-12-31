41b0f22d4404357013bb21e786d04c0077598872
Arda Canbakis <108702450+ardacanbakis@users.noreply.github.com>
2023-12-31 08:13:45 +0300

    Initial commit

---
+++ MineSweeper/src/MineSweeper.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MineSweeper {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Week-3
+ This repository is to upload projects related to Week 3
